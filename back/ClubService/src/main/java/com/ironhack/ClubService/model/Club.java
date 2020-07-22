484eebb08a8d94f71abc62edb6d0941ac7cb22c2
Raquel Rodriguez Gonzalez <rrodriguezgon@icloud.com>
2020-07-22 11:48:03 +0200

    added columns address

--- back/ClubService/src/main/java/com/ironhack/ClubService/model/Club.java
+++ back/ClubService/src/main/java/com/ironhack/ClubService/model/Club.java
@@ -6,6 +6,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
 @Entity
 public class Club {
@@ -15,18 +17,37 @@ public class Club {
     @Column(name = "uuid", unique = true)
     private String id;
 
+
     private String ubication;
+
 
     private String name;
 
+
+    private String city;
+
+
+    private String province;
+
+
     private Integer numberCourts;
+
+
+    private BigDecimal latitude;
+
+
+    private BigDecimal longitude;
 
     public Club(){}
 
-    public Club(String ubication, String name, Integer numberCourts) {
+    public Club(String ubication, String name, String city, String province, Integer numberCourts, BigDecimal latitude, BigDecimal longitude) {
         this.ubication = ubication;
         this.name = name;
+        this.city = city;
+        this.province = province;
         this.numberCourts = numberCourts;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getId() {
@@ -59,5 +80,37 @@ public class Club {
 
     public void setNumberCourts(Integer numberCourts) {
         this.numberCourts = numberCourts;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
     }
 }
